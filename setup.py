2ac93c251b8790f17fb65a44105c4a1e8fa036a2
Oliver K. Ernst <oernst@ucsd.edu>
2023-09-05 00:14:08 -0700

    Bug fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="dash_annotate_cv",
-    version="0.1.3",
+    version="0.1.4",
     author="Oliver K. Ernst",
     description="A Python library for computer vision annotation tasks using Dash",
     long_description=long_description,
