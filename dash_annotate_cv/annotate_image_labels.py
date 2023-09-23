480c19be4470327aa619913dcf3e7c83f1cc63fb
Oliver K. Ernst <oliver.k.ernst@gmail.com>
2023-09-22 18:01:13 -0700

    Rewrote annotate_image_labels to use new controller class

--- dash_annotate_cv/annotate_image_labels.py
+++ dash_annotate_cv/annotate_image_labels.py
@@ -1,3 +1,4 @@
+from dash_annotate_cv.annotate_image_labels_controller import ImageAnnotationController, ImageAnnotationOptions
 from dash_annotate_cv.annotation_storage import AnnotationStorage, AnnotationWriter
 from dash_annotate_cv.helpers import get_trigger_id
 from dash_annotate_cv.image_source import ImageSource, ImageIterator, IndexAboveError, IndexBelowError
@@ -20,30 +21,6 @@ import os
 class AnnotateImageLabelsAIO(html.Div):
     """Annotation component for images
     """
-
-    @dataclass
-    class Options(DataClassDictMixin):
-        """Options
-        """        
-
-        class SelectionMode(Enum):
-            SINGLE = "single"
-            MULTIPLE = "multiple"
-
-        # Selection mode - how many labels can be selected at once
-        selection_mode: SelectionMode = SelectionMode.SINGLE
-
-        # Whether to store timestamps
-        store_timestamps: bool = True
-
-        # Whether to store history
-        store_history: bool = True
-
-        # Whether to use the basename of the image for the annotation
-        use_basename_for_image: bool = False
-
-        # Name of author to store
-        author: Optional[str] = None
 
     # A set of functions that create pattern-matching callbacks of the subcomponents
     class ids:
@@ -92,7 +69,7 @@ class AnnotateImageLabelsAIO(html.Div):
         annotation_storage: AnnotationStorage = AnnotationStorage(),
         annotations_existing: Optional[ImageAnnotations] = None,
         aio_id: Optional[str] = None,
-        options: Options = Options()
+        options: ImageAnnotationOptions = ImageAnnotationOptions()
         ):
         """Constructor
 
@@ -103,15 +80,15 @@ class AnnotateImageLabelsAIO(html.Div):
             annotations_existing (Optional[ImageAnnotations], optional): Existing annotations to continue from, if any. Defaults to None.
             aio_id (Optional[str], optional): IDs for components. Defaults to None.
             options (Options, optional): Options. Defaults to Options().
-        """        
-        self.label_source = label_source
-        self.labels = label_source.get_labels()
-        self.image_source = image_source
-        self.annotations = annotations_existing or ImageAnnotations(image_to_entry={})
-        self.curr_image_name: Optional[str] = None
-        self.options = options
-        self.annotation_writer = AnnotationWriter(annotation_storage)
-        self._curr_image_layout, self._curr_button_layout, self._curr_alert_layout, self.curr_image_idx = None, None, None, None
+        """
+        self.controller = ImageAnnotationController(
+            label_source=label_source,
+            image_source=image_source,
+            annotation_storage=annotation_storage,
+            annotations_existing=annotations_existing,
+            options=options
+            )
+        self._curr_image_layout, self._curr_button_layout, self._curr_alert_layout = None, None, None
 
         # Allow developers to pass in their own `aio_id` if they're
         # binding their own callback to a particular component.
@@ -128,9 +105,15 @@ class AnnotateImageLabelsAIO(html.Div):
 
         super().__init__(self._create_layout(self.aio_id)) # Equivalent to `html.Div([...])`
 
-        self._image_iterator = ImageIterator(self.image_source)
         self._define_callbacks()
     
+    def _refresh_layout(self):
+        # Update layout
+        image = self.controller.curr_image_label.image if self.controller.curr_image_label is not None else None
+        label = self.controller.curr_image_label.label_value if self.controller.curr_image_label is not None else None
+        self._curr_image_layout = self._create_layout_for_image(image)                    
+        self._curr_alert_layout = self._alert_for_existing_label(label)
+
     def _define_callbacks(self):
         """Define callbacks, called in constructor
         """        
@@ -148,80 +131,29 @@ class AnnotateImageLabelsAIO(html.Div):
             trigger_id = get_trigger_id()
             print(f"Trigger: '{trigger_id}'")
 
-            is_initial = trigger_id == "" and self.curr_image_name is None
+            is_initial = trigger_id == "" and self.controller.curr_image_label is None
 
             try:
                 if trigger_id == self.ids.next_submit(MATCH)["subcomponent"] or is_initial:
                     # Submit button was pressed
-
-                    # Store the annotation
-                    if self.curr_image_name is not None and dropdown_value is not None and dropdown_value in self.labels:
-
-                        image_name = os.path.basename(self.curr_image_name) if self.options.use_basename_for_image else self.curr_image_name
-
-                        # Label
-                        label = ImageAnnotations.Annotation.Label(
-                            single=dropdown_value,
-                            timestamp=datetime.datetime.now().timestamp() if self.options.store_timestamps else None,
-                            author=self.options.author
-                            )
-
-                        did_update = False
-                        if image_name in self.annotations.image_to_entry:
-                            ann = self.annotations.image_to_entry[image_name]
-                            if ann.label != label:
-                                ann.label = label
-                                did_update = True
-                        else:
-                            ann = ImageAnnotations.Annotation(
-                                image_name=self.curr_image_name,
-                                label=label
-                                )
-                            self.annotations.image_to_entry[image_name] = ann
-                            did_update = True
-
-                        # Also add history
-                        if did_update and self.options.store_history:
-                            if ann.history is None:
-                                ann.history = []
-                            ann.history.append(label)
-
-                    # Write
-                    self.annotation_writer.write(self.annotations)
-
-                    # Load the next image
-                    self.curr_image_idx, self.curr_image_name, image = self._image_iterator.next()
-                    self._curr_image_layout = self._create_layout_for_image(image)
-                    
-                    # Get annotation of new image
-                    new_dropdown_value = self._get_existing_label_of_curr_image()
-                    self._curr_alert_layout = self._alert_for_existing_label(new_dropdown_value)
+                    self.controller.store_label(dropdown_value)
+                    self._refresh_layout()
 
                 elif trigger_id == self.ids.next_skip(MATCH)["subcomponent"]:
                     # Skip button was pressed
-                    self.curr_image_idx, self.curr_image_name, image = self._image_iterator.next()
-                    self._curr_image_layout = self._create_layout_for_image(image)
-                    new_dropdown_value = self._get_existing_label_of_curr_image()
-                    self._curr_alert_layout = self._alert_for_existing_label(new_dropdown_value)
+                    self.controller.skip()
+                    self._refresh_layout()
 
                 elif trigger_id == self.ids.prev(MATCH)["subcomponent"]:
                     # Previous button was pressed
-                    self.curr_image_idx, self.curr_image_name, image = self._image_iterator.prev()
-                    self._curr_image_layout = self._create_layout_for_image(image)
-                    new_dropdown_value = self._get_existing_label_of_curr_image()
-                    self._curr_alert_layout = self._alert_for_existing_label(new_dropdown_value)
+                    self.controller.previous()
+                    self._refresh_layout()
 
                 elif trigger_id == self.ids.next_missing_ann(MATCH)["subcomponent"]:
                     # Next missing annotation button was pressed
-                    image = None
-                    while self.curr_image_name in self.annotations.image_to_entry:
-                        self.curr_image_idx, self.curr_image_name, image = self._image_iterator.next()
-                    if image is not None:
-                        # Changed image
-                        self._curr_image_layout = self._create_layout_for_image(image)
-                        new_dropdown_value = self._get_existing_label_of_curr_image()
-                        self._curr_alert_layout = self._alert_for_existing_label(new_dropdown_value)
-
+                    self.controller.skip_to_next_missing_ann()
+                    self._refresh_layout()
+                
                 elif trigger_id == self.ids.dropdown(MATCH)["subcomponent"]:
                     # Dropdown was changed
                     pass
@@ -229,7 +161,7 @@ class AnnotateImageLabelsAIO(html.Div):
                 else:
                     print(f"Unknown button pressed: {trigger_id}")
 
-                if new_dropdown_value is not None and new_dropdown_value in self.labels:
+                if new_dropdown_value is not None and new_dropdown_value in self.controller.labels:
                     self._curr_button_layout = self._create_layout_buttons(
                         aio_id=self.aio_id, 
                         curr_selected_label=new_dropdown_value,
@@ -281,20 +213,11 @@ class AnnotateImageLabelsAIO(html.Div):
     
     def _alert_for_existing_label(self, existing_label: Optional[str]) -> Optional[dbc.Alert]:
         """Create layout for existing label
-        """        
-        if existing_label is not None and existing_label in self.labels:
+        """
+        if existing_label is not None and existing_label in self.controller.labels:
             return dbc.Alert(f"Existing annotation: {existing_label}", color="primary")
         else:
             return None
-
-    def _get_existing_label_of_curr_image(self) -> Optional[str]:
-        """Get existing label of current image
-        """        
-        if self.curr_image_name in self.annotations.image_to_entry:
-            entry = self.annotations.image_to_entry[self.curr_image_name]
-            if entry.label.single is not None and entry.label.single in self.labels:
-                return entry.label.single
-        return None
 
     def _create_layout(self, aio_id: str):
         """Create layout for component
@@ -335,19 +258,19 @@ class AnnotateImageLabelsAIO(html.Div):
         if not enable_dropdown:
             style_dropdown["display"] = "none"
 
-        if hasattr(self,"_image_iterator") and self._image_iterator is not None and self.curr_image_idx is not None:
-            no_images = self._image_iterator.no_images
-            title = f"Image {self.curr_image_idx+1}/{no_images}"
+        if self.controller.curr_image_label is not None:
+            no_images = self.controller.no_images
+            title = f"Image {self.controller.curr_image_label.image_idx+1}/{no_images}"
         else:
             title = "Image"
 
         # Create components
         dropdown = dcc.Dropdown(
-            self.labels, 
+            self.controller.labels, 
             value=curr_selected_label, 
             id=self.ids.dropdown(aio_id), 
             style=style_dropdown,
-            multi=self.options.selection_mode == AnnotateImageLabelsAIO.Options.SelectionMode.MULTIPLE
+            multi=self.controller.options.selection_mode == ImageAnnotationOptions.SelectionMode.MULTIPLE
             )
         prev_button = dbc.Button("Previous image", color="dark", id=self.ids.prev(aio_id), style=style_prev)
         next_button = dbc.Button("Next (save)", color="success", id=self.ids.next_submit(aio_id), style=style_next_save)
@@ -375,9 +298,11 @@ class AnnotateImageLabelsAIO(html.Div):
             ])
         ])
 
-    def _create_layout_for_image(self, image: Image.Image):
+    def _create_layout_for_image(self, image: Optional[Image.Image]):
         """Create layout for the image
         """        
+        if image is None:
+            return []
         fig = px.imshow(image)
         fig.update_layout(margin=dict(l=0, r=0, b=0, t=0))
         return dcc.Graph(id="graph-styled-annotations", figure=fig)
--- dash_annotate_cv/annotate_image_labels_controller.py
+++ dash_annotate_cv/annotate_image_labels_controller.py
@@ -4,38 +4,41 @@ from dash_annotate_cv.image_source import ImageSource, ImageIterator
 from dash_annotate_cv.label_source import LabelSource
 
 from dataclasses import dataclass
-from typing import Optional, Tuple
+from typing import Optional, List
 from PIL import Image
 from enum import Enum
 from mashumaro import DataClassDictMixin
 import os
 import datetime
+from collections import namedtuple
+
+ImageLabel = namedtuple("ImageLabel", ["image_idx", "image_name", "image", "label_value"])
+
+@dataclass
+class ImageAnnotationOptions(DataClassDictMixin):
+    """Options
+    """        
+
+    class SelectionMode(Enum):
+        SINGLE = "single"
+        MULTIPLE = "multiple"
+
+    # Selection mode - how many labels can be selected at once
+    selection_mode: SelectionMode = SelectionMode.SINGLE
+
+    # Whether to store timestamps
+    store_timestamps: bool = True
+
+    # Whether to store history
+    store_history: bool = True
+
+    # Whether to use the basename of the image for the annotation
+    use_basename_for_image: bool = False
+
+    # Name of author to store
+    author: Optional[str] = None
 
 class ImageAnnotationController:
-
-    @dataclass
-    class Options(DataClassDictMixin):
-        """Options
-        """        
-
-        class SelectionMode(Enum):
-            SINGLE = "single"
-            MULTIPLE = "multiple"
-
-        # Selection mode - how many labels can be selected at once
-        selection_mode: SelectionMode = SelectionMode.SINGLE
-
-        # Whether to store timestamps
-        store_timestamps: bool = True
-
-        # Whether to store history
-        store_history: bool = True
-
-        # Whether to use the basename of the image for the annotation
-        use_basename_for_image: bool = False
-
-        # Name of author to store
-        author: Optional[str] = None
 
     def __init__(
         self,
@@ -43,7 +46,7 @@ class ImageAnnotationController:
         image_source: ImageSource,
         annotation_storage: AnnotationStorage = AnnotationStorage(),
         annotations_existing: Optional[ImageAnnotations] = None,
-        options: Options = Options()
+        options: ImageAnnotationOptions = ImageAnnotationOptions()
         ):
         self.options = options
         self.annotation_writer = AnnotationWriter(annotation_storage)
@@ -52,58 +55,89 @@ class ImageAnnotationController:
         self._labels = label_source.get_labels()
         self.annotations = annotations_existing or ImageAnnotations(image_to_entry={})
         self._image_iterator = ImageIterator(self.image_source)
-        self._curr_image_idx = None
-        self._curr_image_name: Optional[str] = None
+        self._curr_image_label: Optional[ImageLabel] = None
+    
+    @property
+    def no_images(self) -> int:
+        return self._image_iterator.no_images
+
+    @property
+    def labels(self) -> List[str]:
+        return list(self._labels)
+
+    @property
+    def curr_image_label(self) -> Optional[ImageLabel]:
+        return self._curr_image_label
 
     def store_label(self, label_value: Optional[str]):
 
-        if self._curr_image_name is None or label_value is None or not label_value in self._labels:
-            return
+        if self._curr_image_label is not None and label_value is not None and label_value in self._labels:
+                
+            # Store the annotation
+            image_name = os.path.basename(self._curr_image_label.image_name) if self.options.use_basename_for_image else self._curr_image_label.image_name
 
-        # Store the annotation
-        image_name = os.path.basename(self._curr_image_name) if self.options.use_basename_for_image else self._curr_image_name
-
-        # Label
-        label = ImageAnnotations.Annotation.Label(
-            single=label_value,
-            timestamp=datetime.datetime.now().timestamp() if self.options.store_timestamps else None,
-            author=self.options.author
-            )
-
-        did_update = False
-        if image_name in self.annotations.image_to_entry:
-            ann = self.annotations.image_to_entry[image_name]
-            if ann.label != label:
-                ann.label = label
-                did_update = True
-        else:
-            ann = ImageAnnotations.Annotation(
-                image_name=image_name,
-                label=label
+            # Label
+            label = ImageAnnotations.Annotation.Label(
+                single=label_value,
+                timestamp=datetime.datetime.now().timestamp() if self.options.store_timestamps else None,
+                author=self.options.author
                 )
-            self.annotations.image_to_entry[image_name] = ann
-            did_update = True
 
-        # Also add history
-        if did_update and self.options.store_history:
-            if ann.history is None:
-                ann.history = []
-            ann.history.append(label)
+            did_update = False
+            if image_name in self.annotations.image_to_entry:
+                ann = self.annotations.image_to_entry[image_name]
+                if ann.label != label:
+                    ann.label = label
+                    did_update = True
+            else:
+                ann = ImageAnnotations.Annotation(
+                    image_name=image_name,
+                    label=label
+                    )
+                self.annotations.image_to_entry[image_name] = ann
+                did_update = True
 
-        # Write
-        self.annotation_writer.write(self.annotations)
+            # Also add history
+            if did_update and self.options.store_history:
+                if ann.history is None:
+                    ann.history = []
+                ann.history.append(label)
 
-    def load_next_image(self) -> Tuple[int, str, Image.Image, str]:
+            # Write
+            self.annotation_writer.write(self.annotations)
+
         # Load the next image
-        self._curr_image_idx, self._curr_image_name, image = self._image_iterator.next()
-        
-        # Get annotation of new image
+        image_idx, image_name, image = self._image_iterator.next()
         label_value = self._get_existing_label_of_curr_image()
-        return self._curr_image_idx, self._curr_image_name, image, label_value
+        self._curr_image_label = ImageLabel(image_idx, image_name, image, label_value)
+
+    def skip(self):
+        image_idx, image_name, image = self._image_iterator.next()
+        label_value = self._get_existing_label_of_curr_image()
+        self._curr_image_label = ImageLabel(image_idx, image_name, image, label_value)
+
+    def previous(self):
+        image_idx, image_name, image = self._image_iterator.prev()
+        label_value = self._get_existing_label_of_curr_image()
+        self._curr_image_label = ImageLabel(image_idx, image_name, image, label_value)
+
+    def skip_to_next_missing_ann(self) -> Optional[ImageLabel]:
+        image, image_idx = None, None
+        image_name = self._curr_image_label.image_name if self._curr_image_label is not None else None
+        while image_name in self.annotations.image_to_entry:
+            image_idx, image_name, image = self._image_iterator.next()
+        if image is not None and image_idx is not None and image_name is not None:
+            # Changed image
+            label_value = self._get_existing_label_of_curr_image()
+            return ImageLabel(image_idx, image_name, image, label_value)
+        return None
 
     def _get_existing_label_of_curr_image(self) -> Optional[str]:
-        if self._curr_image_name in self.annotations.image_to_entry:
-            entry = self.annotations.image_to_entry[self._curr_image_name]
+        if self._curr_image_label is None:
+            return None
+        image_name = self._curr_image_label.image_name
+        if image_name in self.annotations.image_to_entry:
+            entry = self.annotations.image_to_entry[image_name]
             if entry.label.single is not None and entry.label.single in self._labels:
                 return entry.label.single
         return None
